12efe668a3827c6e55b935bd41785d1b4ab14164
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-07-21 13:00:55 +0530

    "JAVA Codes added Lec 1-12"

---
+++ CWH JAVA Full Course/CWHLectureCodes/Lecture4.java
@@ -0,0 +1,34 @@
+package CWHLectureCodes;
+
+public class Lecture4 {
+    public static void main(String[] args){
+ // Literals = constant value which can be assigned to a variable
+ byte age1 = 34;
+ System.out.println(age1);
+ int age2 = 56;
+ System.out.println(age2);
+ long age3 = 344444444, age4 = 344444444L , age5 = 344444444l ; //(L/l ) not compulsory for modern compilers
+ System.out.println(age3);
+ System.out.println(age4);
+ System.out.println(age5);
+ short age6 = 3444;
+ System.out.println(age6);
+ char ch = 'A';
+ System.out.println(ch);
+ float f1 = 5.66f , f2 = 5.66F ;//(f/F) Compulsory
+ double d1 = 4.66 , d2 = 4.66D; //(D/d) Not compulsory
+ System.out.println(d1);
+ System.out.println(d2);
+ System.out.println(f1);
+ System.out.println(f2);
+ boolean b1 = true  , b2 = false ;
+ System.out.println(b1);
+ System.out.println(b2);
+ String name = "Koushik";
+ System.out.println(name);
+
+
+    }
+}
+
+        
