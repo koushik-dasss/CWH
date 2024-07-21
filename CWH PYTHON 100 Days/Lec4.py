32b6d7e63615b0877007a1e77c1ce7327059e4ed
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-07-21 19:42:54 +0530

    Initial Python Code dump

---
+++ CWH PYTHON 100 Days/Lec4.py
@@ -0,0 +1,13 @@
+print("Hello World") # Hello World
+print(8)
+a = 10 
+print(a) # 10 
+print(type(a))   #int 
+print(type(10))  #int 
+
+print(17*13) # 221
+print(17/13) # 1.307
+print(17//13) # 1 (int division , floor division )
+print(17+13) # 30 
+print(17-13) # 4 
+
---
+++ CWH PYTHON 100 Days/Lec5.py
@@ -0,0 +1,27 @@
+# This is the basic lecture 
+# Ctrl + / is  for commenting 
+print("My name is Koushik Das\nand I am from Odisha ")
+
+print("My name is Koushik Das\tand I am from Odisha ")
+''' Multi line comment 
+Hello
+Koushik
+Das 
+'''
+""" Multi line comment 
+Hello
+Koushik
+Das 
+"""
+
+#  New line character is to be done with the help of strings for getting the required output
+print("Hello I am a \"good boy\"")
+
+# \"\" and \'\' 
+print("Hey",6,7)
+print("Hey",6,7,sep="_")
+print("Hey",6,7,sep="_",end="009\n")
+print("Koushik")
+
+
+
