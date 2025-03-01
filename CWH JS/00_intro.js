63320b8c06f8464e5370701487ebeba940963b6b
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-03-01 23:52:49 +0530

    intro of js

---
+++ CWH JS/00_intro.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+function addThreeNumbers(a,b,c){
+    return a + b + c ;
+}
+let a = 20  , b = 30 , c = 40;
+let sum = addThreeNumbers(a,b,c);
+console.log("Sum of 3 numbers is : "+ sum); // sum of 3 numbers using function
+
