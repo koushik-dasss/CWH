d3a8d8b1c7d60939b52832e9fe52b0a91765fcb2
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-03-02 23:54:36 +0530

    intro and variables

---
+++ CWH JS/02_variables.js
@@ -0,0 +1,11 @@
+console.log("This is the first line in the code that is going to be printed");
+// var a = 67;
+// don't use var , use let or const
+let a = 67; // 67 is a literal here
+console.log(a); // number 
+a = "Koushik"; // changed data type at runtime as it's a dynamically typed language 
+console.log(a); // string
+// remember rules for variable declaration 
+// js is case sensiitve but it affects readability 
+
+
