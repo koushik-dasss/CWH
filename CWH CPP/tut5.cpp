cd39c92f3030bd85e9ae8c10aac6edcfe0baf37f
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-11-14 00:23:02 +0530

    io stream insertion and extraction

---
+++ CWH CPP/tut5.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a , b ;
+    cout<<"Enter two numbers :"<<endl; // insertion operator <<
+    cin>>a>>b; // extraction operator >>
+    cout<<"The sum of "<<a<<" and "<<b<<" is: "<<(a+b)<<endl;
+    return 0;
+}
